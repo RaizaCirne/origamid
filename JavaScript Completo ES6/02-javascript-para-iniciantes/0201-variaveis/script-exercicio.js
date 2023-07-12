6bc69778ceda06acaed4a54f2492e35a07ffa0e8
RaizaCirne <raizabraz93@gmail.com>
2023-07-12 16:10:05 -0300

    feat: add exercício variavéis

---
+++ JavaScript Completo ES6/02-javascript-para-iniciantes/0201-variaveis/script-exercicio.js
@@ -0,0 +1,24 @@
+// Declarar uma variável com o seu nome
+var nome = 'Raíza';
+
+// Declarar uma variável com a sua idade
+var idade = 29;
+
+// Declarar uma variável com a sua comida
+// favorita e não atribuir valor
+var comidaFavorita; 
+
+// Atribuir valor a sua comida favorita
+
+comidaFavorita = 'Comida Japonêsa';
+console.log(comidaFavorita)
+
+// Declarar 5 variáveis diferentes sem valores
+
+var cantoraFavorita, 
+    endereco, 
+    nomeNamorada, 
+    idadeNamorada, 
+    anoNascimentoMae; 
+
+console.log(nome, idade, comidaFavorita);
