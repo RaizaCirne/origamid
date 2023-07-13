e6de290ee602b80b8b334cb83d98370f5b3bdaca
RaizaCirne <raizabraz93@gmail.com>
2023-07-13 12:03:17 -0300

    feat: atualizar tipos de dados

---
+++ JavaScript Completo ES6/02-javascript-para-iniciantes/0202-tipos-de-dados/0202-tipos-de-dados_pratica/script-exercicio.js
@@ -0,0 +1,34 @@
+// Declare uma variável contendo uma string
+var tipoDeInstrumento = 'violão';
+console.log(tipoDeInstrumento);
+console.log(typeof tipoDeInstrumento);
+
+// Declare uma variável contendo um número dentro de uma string
+var acertosProva = '8'; 
+console.log(typeof acertosProva);
+
+// Declare uma variável com a sua idade
+var idadeRaiza = 29;
+console.log(idadeRaiza);;
+
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var nome = 'Raíza';
+var sobrenome = 'Cirne'; 
+var nomeCompleto = `${nome} ${sobrenome}`;
+console.log(nomeCompleto);
+
+// Coloque a seguinte frase em uma variável: It's time
+// três formas por causa das aspas
+var frase = 'It\´s time';
+var frase = `It´s time`; 
+var frase = "It´s time";
+console.log(frase);
+
+// Verifique o tipo da variável que contém o seu nome
+// Duas formas: 
+console.log(typeof nome);
+// ou 
+var verificarTipoNome = typeof nome; 
+console.log(verificarTipoNome);
+
---
+++ JavaScript Completo ES6/02-javascript-para-iniciantes/0202-tipos-de-dados/0202-tipos-de-dados_pratica/script.js
@@ -0,0 +1,44 @@
+// // typeof -> verifica o que a variável é 
+// var nome = 'André';
+// var idade = 28;
+// var simbolo = Symbol();
+// console.log(typeof simbolo);
+
+
+// // soma de strings - concatenação
+// var nome = 'André';
+// var sobrenome = 'Rafael';
+// var nomeCompleto = nome + ' ' + sobrenome; 
+// console.log(nomeCompleto);
+
+// // soma de número com string será string
+// var gols = 1000;
+// var frase = 'Romário fez ' + gols + ' gols';
+// console.log(typeof frase);
+
+// // string + número - concatenação _> 20188
+// var ano = '2018';
+// var mes = 8; 
+// console.log(ano + mes);
+
+// // número + número - soma -> 2016 
+// var ano = 2018;
+// var mes = 8; 
+// console.log(ano + mes);
+
+// // uso de aspas 
+// var melhor = 'teste';
+// var frase1 = 'Esse é o \"mehor\" jogo';
+// console.log(frase1);
+
+// // Utilizando Template String - usa aspas diferente
+// var gols = 1000;
+// var frase1 = 'Romário fez ' + gols + ' gols';
+// var frase2 = `Romário fez ${gols} gols`; 
+// console.log(frase2);
+
+// //uso de operadores dentro da Template String
+// // Você deve passar expressões / variáveis dentro de ${}
+// var frase3 = `Romário fez ${gols * 2} gols`; 
+// console.log(frase3); 
+
