7aa51cd3fac367f0c513b78d599a8d1d91be3f1b
RaizaCirne <raizabraz93@gmail.com>
2023-07-13 13:38:21 -0300

    feat: add numeros e operadores

---
+++ JavaScript Completo ES6/02-javascript-para-iniciantes/0203-numeros-e-operadores/0203-numeros-e-operadores_pratica/script-exercicio.js
@@ -0,0 +1,28 @@
+// Qual o resultado da seguinte expressão?
+var total = 10 + 5 * 2 / 2 + 20; // 35
+console.log(total);
+
+
+// Crie duas expressões que retornem NaN
+var idade = 20; 
+var nome = 'Raíza';
+var expressao = nome / idade; 
+console.log(expressao);
+
+// Somar a string '200' com o número 50 e retornar 250
+
+var n1 = +'200'
+var n2 = 50
+var soma = n1 + n2;
+console.log(soma);
+
+// Incremente o número 5 e retorne o seu valor incrementado
+var x = 5; 
+console.log(++x);
+
+// Como dividir o peso por 2?
+var numero = +'80' / 2;
++numero;
+var unidade = 'kg';
+var peso = numero + unidade; // '80kg'
+console.log(+peso);
