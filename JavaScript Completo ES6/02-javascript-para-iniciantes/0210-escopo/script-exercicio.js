124caeb5eed5acecb21a7fc1d72d08193a471243
RaizaCirne <raizabraz93@gmail.com>
2023-07-17 16:14:16 -0300

    feat: add exercicio escopo

---
+++ JavaScript Completo ES6/02-javascript-para-iniciantes/0210-escopo/script-exercicio.js
@@ -0,0 +1,31 @@
+// // Por qual motivo o código abaixo retorna com erros?
+{
+  var cor = 'preto';
+  const marca = 'Fiat';
+  let portas = 4;
+  console.log(cor, marca, portas); 
+  // porque o var está sendo colocado no console.log
+  //colocar o console.log dentro do escopo para pegar o const e o let que não pega fora 
+}
+
+// Como corrigir o erro abaixo?
+const dois = 2;
+function somarDois(x) {
+  return x + dois;
+}
+function dividirDois(x) {
+  return x / dois;
+}
+console.log(somarDois(4));
+console.log(dividirDois(10));
+
+
+// // O que fazer para total retornar 500?
+const numero = 50;
+
+for(let numero = 0; numero < 10; numero++) {
+  console.log(numero);
+}
+
+const total = 10 * numero;
+console.log(total);
