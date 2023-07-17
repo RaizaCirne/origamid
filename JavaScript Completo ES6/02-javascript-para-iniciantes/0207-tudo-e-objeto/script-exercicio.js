82f82c2ac27e6699d5f6c9cf63a73fa6e9315346
RaizaCirne <raizabraz93@gmail.com>
2023-07-17 09:51:03 -0300

    feat: add exercicio objetos

---
+++ JavaScript Completo ES6/02-javascript-para-iniciantes/0207-tudo-e-objeto/script-exercicio.js
@@ -0,0 +1,26 @@
+// nomeie 3 propriedades ou métodos de strings
+
+var nome = 'Raíza'; 
+
+nome.length // Descobre o comprimento de uma string
+nome.toLowerCase // converte os caracteres para minusculo
+nome.toUpperCase // converte os caracteres para maiúsculo
+nome = nome.replace('Ra', 'Ta') // substitui uma substring dentro de uma string com uma outra substring
+
+
+// nomeie 5 propriedades ou métodos de elementos do DOM
+
+var btn = document.querySelector('.btn')
+  //Propriedades
+
+  // documentElement -> Retorna o elemento que é filho direto do documento. 
+  // hidden -> propriedade read-only retorna um valor booleano indicando se a página é considerada oculta ou não.
+
+  // Métodos
+
+  // createElement -> Cria um novo elemento com o nome de tag fornecido.
+  // getElementsByClassName 
+
+
+// busque na web um objeto (método) capaz de interagir com o clipboard, 
+// clipboard é a parte do seu computador que lida com o CTRL + C e CTRL + V
