b27588e33d3b5cd69a919eb9bfd8fd4d000ddb1d
Raiza Cirne <raizacirne@gmail.com>
2023-12-24 22:15:56 -0300

    Feat: add function

--- orig_tst/js/script.js
+++ orig_tst/js/script.js
@@ -1,16 +1,23 @@
 // Adicionar JS ao projeto
-function soma (a,b){
-  return a + b;
-}
+// function soma (a,b){
+//   return a + b;
+// }
 
-console.log(soma(2,3));
+// console.log(soma(2,3));
 
 
 // multiplicacao
-function multiplicacao (a,b){
-  return a * b;
+// function multiplicacao (a,b){
+//   return a * b;
+// }
+
+// console.log(multiplicacao(2,3));
+
+
+function natal () {
+  return "Feliz Natal"
 }
 
-console.log(multiplicacao(2,3));
+console.log(natal());
 
 
